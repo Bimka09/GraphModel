b4619f4a29590ca12c8473320c26a5e10d5d8112
Bimka09 <90Alex09@mail.ru>
2022-10-21 23:51:00 +0300

    Добавьте файлы проекта.

---
+++ GraphModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Graph
+{
+    public class GraphModel
+    {
+        List<Vertex> Vertexes = new List<Vertex>();
+        List<Edge> Edges = new List<Edge>();
+
+        public int VertexCount => Vertexes.Count;
+        public int EdgeCount => Edges.Count;
+
+        public void AddVertex(Vertex vertex)
+        {
+            Vertexes.Add(vertex);
+        }
+
+        public void AddEdge(Vertex from, Vertex to)
+        {
+            var edge = new Edge(from, to);
+            Edges.Add(edge);
+        }
+        public int[,] GetMatrix()
+        {
+            var matrix = new int[VertexCount, VertexCount];
+            foreach(var edge in Edges)
+            {
+                var row = edge.From.Number;
+                var column = edge.To.Number;
+
+                matrix[row - 1, column - 1] = edge.Weight;
+                if(row == column)
+                {
+                    matrix[column - 1, row - 1] = edge.Weight * 2;
+                }
+                else
+                {
+                    matrix[column - 1, row - 1] = edge.Weight;
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Graph;
+
+var graph = new GraphModel();
+
+var v1 = new Vertex(1);
+graph.AddVertex(v1);
+var v2 = new Vertex(2);
+graph.AddVertex(v2);
+var v3 = new Vertex(3);
+graph.AddVertex(v3);
+var v4 = new Vertex(4);
+graph.AddVertex(v4);
+var v5 = new Vertex(5);
+graph.AddVertex(v5);
+
+graph.AddEdge(v1, v2);
+graph.AddEdge(v2, v3);
+graph.AddEdge(v2, v4);
+graph.AddEdge(v3, v4);
+graph.AddEdge(v5, v5);
+
+var matrix = graph.GetMatrix();
+Console.Write(" ");
+for (int i = 0; i < graph.VertexCount; i++)
+{
+    Console.Write(" | " + i + " | ");
+}
+Console.WriteLine();
+Console.WriteLine("------------------------------------");
+for (int i = 0; i < graph.VertexCount; i++)
+{
+    Console.Write(i + 1);
+    for(int j = 0; j < graph.VertexCount; j++)
+    {
+        Console.Write(" | " + matrix[i, j] + " | ");
+    }
+    Console.WriteLine();
+}
